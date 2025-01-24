045b925642012c8a900c741c4caba615cde157c4
rahim8050 <rahimranxx8050@gmail.com>
2025-01-24 18:38:43 +0000

    updates

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\notecontroller;
 use App\Http\Controllers\welcomecontroller;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [welcomecontroller::class,'welcome'])->name('welcome');
+Route::get('/', [welcomecontroller::class,'welcome'])->name('welcome');
 // Route::get('/note',[notecontroller::class,'index'] )->name('note.index');
 // Route::get('/note/create',[notecontroller::class,'create'] )->name('note.create');
 // Route::post('/note',[notecontroller::class,'store'] )->name('note.store');
